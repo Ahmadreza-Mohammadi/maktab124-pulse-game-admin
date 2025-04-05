88eb3a306e84d22dacd15d0b338dd96c12462503
noway777 <ahmadrezamohammadi777@gmail.com>
2025-04-06 02:00:32 +0330

    feat(inventory): table added with filter bar

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,13 @@
+import { useNavigate } from "react-router";
+import { routes } from "../../router/const";
+
 function Header() {
+  const navigate = useNavigate();
   return (
-    <div className="navbar text-white py-12 px-8 flex justify-between items-center sticky top-0 z-10 shadow-lg after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:h-[3px] after:bg-blue-500 after:transition-all after:duration-1000 hover:after:w-full">
+    <div className="navbar text-white py-12 px-8 flex justify-between items-center sticky top-0 z-10 shadow-lg bg-gradient-to-r from-gray-800 via-cyan-900 to-gray-800 after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:h-[3px] after:bg-cyan-500 after:transition-all after:duration-1000 hover:after:w-full transition-all duration-500 hover:shadow-[0_0_25px_rgba(59,130,246,0.4)]">
       <div className="flex items-center gap-6 backdrop-blur-sm p-2 rounded-lg">
         <img
+          onClick={() => navigate(routes.profile)}
           className="h-14 cursor-pointer transform hover:scale-110 hover:shadow-[0_0_15px_rgba(59,130,246,0.7)] transition-all duration-300"
           src="https://www.svgrepo.com/show/408429/user-person-profile-block-account-circle.svg"
           alt="Profile"
